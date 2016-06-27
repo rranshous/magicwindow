bb7a076960d0b8f1eb27be59c827a384546fb3dc
Robby Ranshous <rranshous@gmail.com>
2016-06-27 14:13:05 -0400

    auto refresh images page

--- serve_images.rb
+++ serve_images.rb
@@ -12,11 +12,12 @@ def create_data_uri(source_file, mime_type)
 end
 
 get '/' do
-  to_return = ''
+  to_return = '<html><head><meta http-equiv="refresh" content="10"/></head><body>'
   Dir["#{DATA_DIR}/*.jpeg"].each do |path|
     puts "reading in file: #{path}"
     to_embed = create_data_uri(path, 'image/jpeg')
     to_return += "<img style='float: left; max-height: 400px; max-width: 400px;' src='#{to_embed}'/>"
   end
+  to_return += '</body></html>'
   to_return
 end
